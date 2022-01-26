ee55ac897b028458f50b5a20af1bd13d23ae5cc4
au <eu2552@mail.taipei.gov.tw>
2022-01-26 18:13:42 +0800

    deploy

---
+++ precache-manifest.50a3a8c708be941b4ff00db5effa520b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "9eab9aeeedb11fe7452f",
+    "url": "/disappearing_market/js/about.9a28a535.js"
+  },
+  {
+    "revision": "48de096926c8727ee15d",
+    "url": "/disappearing_market/css/app.91c84a3a.css"
+  },
+  {
+    "revision": "48de096926c8727ee15d",
+    "url": "/disappearing_market/js/app.31ffdad8.js"
+  },
+  {
+    "revision": "0d430e497bfeae13f28e",
+    "url": "/disappearing_market/css/chunk-vendors.7c6a3be5.css"
+  },
+  {
+    "revision": "0d430e497bfeae13f28e",
+    "url": "/disappearing_market/js/chunk-vendors.05ed4221.js"
+  },
+  {
+    "revision": "7390598e247ae7f3ac4fcfdec67812a0",
+    "url": "/disappearing_market/img/1.7390598e.png"
+  },
+  {
+    "revision": "8f8cf4cd6ce41295607995b28c58bf81",
+    "url": "/disappearing_market/img/rectangle-slider.8f8cf4cd.svg"
+  },
+  {
+    "revision": "8f03eb046719084f0b3b5b1bb8adfb4d",
+    "url": "/disappearing_market/img/flower.8f03eb04.svg"
+  },
+  {
+    "revision": "23142e7d4235666453e45e4bf1cb00ac",
+    "url": "/disappearing_market/img/farm.23142e7d.svg"
+  },
+  {
+    "revision": "a26a2c8b488a381cce324382c4795998",
+    "url": "/disappearing_market/img/toy.a26a2c8b.svg"
+  },
+  {
+    "revision": "a20fb27f09ace1fe5f7d81e9198e7129",
+    "url": "/disappearing_market/img/building.a20fb27f.svg"
+  },
+  {
+    "revision": "6b84bc74567079c4e8db4f840e52d183",
+    "url": "/disappearing_market/img/mix.6b84bc74.svg"
+  },
+  {
+    "revision": "88c38bdc6f8a183f915ded830d225ef3",
+    "url": "/disappearing_market/img/twitter.88c38bdc.svg"
+  },
+  {
+    "revision": "422e766543a3850de8db1f049775a1e9",
+    "url": "/disappearing_market/img/facebook.422e7665.svg"
+  },
+  {
+    "revision": "9a39a665b8dbc082df78dd3f8ca93be2",
+    "url": "/disappearing_market/img/morning.9a39a665.svg"
+  },
+  {
+    "revision": "2e4d2ad9163375c0c0901c6a5776d808",
+    "url": "/disappearing_market/img/2.2e4d2ad9.png"
+  },
+  {
+    "revision": "7f47ca3f7d8c3e2d72867b2a6a77b9c4",
+    "url": "/disappearing_market/img/car.7f47ca3f.svg"
+  },
+  {
+    "revision": "4dcf3ae6e6c797fcf2dc3e44ec715cae",
+    "url": "/disappearing_market/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/disappearing_market/robots.txt"
+  }
+];
